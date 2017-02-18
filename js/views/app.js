13622461e25c9718e8efdd44b39231660564c31b
Si White <info@silentdesigns.co.nz>
2017-02-18 22:10:30 +1300

    implemented map and markers, started json list of stores

--- js/views/app.js
+++ js/views/app.js
@@ -5,52 +5,71 @@ var AppView = Backbone.View.extend({
 
 	initialize: function() {
 		this.initMap();
-		this.getEvents();
-
-		this.map;
+		this.getEvents();	
 	},
 
 	initMap: function() {
-		var home = {lat: -41.3222902, lng: 174.7743656};
 		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -41.3222902, lng: 174.7415353},
+			center: {lat: -42.0155275, lng: 173.6358103},
 			scrollwheel: false,
-			zoom: 12
-		});
-		var marker = new google.maps.Marker({
-			position: home,
-			map: map
+			zoom: 5
 		});
 	},
 
 	getEvents: function() {
-		var username = 'silentdesigns';
-		var password = 'vbkmr6d28vjw';
+		var geocoder = new google.maps.Geocoder();
 		var that = this;
+
 		$.ajax({
-			type: "GET",
-			url: 'http://api.eventfinda.co.nz/v2/events.json',
-			dataType: "jsonp",
-			timeout: 5000,
-			beforeSend: function (data) {
-				data.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-			},
+			type: "GET",	
+			url: "http://local.googlemaps.com:8888/js/stores.json",
+			dataType: 'json',
 			success: function (data) {
-				// console.log(data.events)
-				$.each(data.events, function(index, event) {
-					console.log(event.name, event.address);
-					that.geoCodeAddress(event.address);
-          		});
+				//console.log(data.regions);
+				
+				$.each(data.regions, function(i, region) {
+					console.log(region.name);
+					$('select').append('<option>'+region.name+'</option>');
+					// $.each(region, function(key, val) {
+					// 	//console.log(region[key]);
+					// 	$('select').append('<option>'+region[key]+'</option>');
+					// });
+					
+					// $.each(region.Auckland, function(i, store) {
+					// 	//console.log(store);
+					// 	that.addMarker(map, store.lat, store.lng, store.address, store.name, store.phone);
+					// });
+				});
 			},
-			error: function(data, textStatus, errorThrown) {
-				console.log('error');
+			error: function(xhr, status, error) {
+					console.log(xhr, status, error);
 			}
 		});
 	},
 
-	geoCodeAddress: function(address) {
-		console.log('geocode', address);
-		//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+	addMarker: function(resultsMap, lat, lng, address, name, phone) {
+		var key = 'AIzaSyAGcse28lYcou-lUbmnLGRFGaGbElgFsPw';
+		var iconBase = 'images/';
+		var contentString = '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h3>'+name+'</h3>'+
+		'<div id="bodyContent">'+
+		'<p>'+address+'</p>'+
+		'<p>'+phone+'</p>'+
+		'</div>'+
+		'</div>';
+		var marker = new google.maps.Marker({
+			map: resultsMap,
+			position: {lat: lat, lng: lng},
+			icon: iconBase + 'icon.png'
+		});
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+		marker.addListener('click', function() {
+			infowindow.open(resultsMap, marker);
+		});
 	}
 });
 
